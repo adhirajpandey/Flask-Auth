8fcb71190a878044f0f361c0e77cf064a538fa60
adhirajpandey <pandey.adhiraj02@gmail.com>
2024-01-05 22:30:43 +0530

    Add Logout Button

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 import dao
+import os
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 
 @app.route("/", methods = ["GET"])
@@ -32,20 +34,35 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         
+        if not dao.check_if_username_exists(username):
+            login_error = "Username does not exist"
+            return render_template("login.html", error = login_error)
+
         user_details = dao.fetch_user_by_username(username)
 
         if user_details:
             if user_details[2] == password:
+                session['username'] = username
                 return redirect(url_for('content'))
             else:
-                login_error = "Incorrect Password"
+                login_error = "Incorrect Password"  
                 return render_template("login.html", error = login_error)
 
     return render_template("login.html")
 
 @app.route("/content", methods = ["GET"])
 def content():
-    return render_template("content.html")
+    if 'username' in session:
+        return render_template("content.html", username = session['username'])
+    else:
+        return redirect(url_for('login'))
 
+@app.route("/logout", methods = ["GET"])
+def logout():
+    if 'username' in session:
+        session.pop('username', None)
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('index'))
 
 app.run(debug=True)
